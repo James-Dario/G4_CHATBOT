74bf03d680055942e6bbedc7636b4e97dd15356c
Ashley May M. Hernandez <90459331+Leyliiii@users.noreply.github.com>
2022-10-07 08:29:57 +0800

    Add files via upload

---
+++ PISO Initial Design/chatbot.js
@@ -0,0 +1,56 @@
+const chatBody = document.querySelector(".chat-body");
+const txtInput = document.querySelector("#txtinput");
+const sendB = document.querySelector(".send");
+const loadingElement = document.querySelector(".loading");
+
+sendB.addEventListener("click", () => renderUserMessage());
+
+txtInput.addEventListener("keyup", ()=>{
+    if(event.keyCode === 13){
+        renderUserMessage();
+    }
+});
+const renderUserMessage = () => {
+    const userInput = txtInput.value;
+    renderMessageElement(userInput, "user");
+    txtInput.value = "";
+    toggleLoading(false);
+
+    setTimeout(()=>{
+        renderChatbotResponse(userInput);
+        setScrollPos();
+        toggleLoading(true);
+    }, 1200);
+};
+
+const renderChatbotResponse = (userInput) =>{
+    const res = getChatbotResponse(userInput);
+    renderMessageElement(res);
+};
+
+const renderMessageElement = (txt, type) => {
+    let className = "user-message";
+    if(type!=='user'){
+        className = "chatbot-message";
+    }
+    const messageElement = document.createElement("div");
+    const txtNode = document.createTextNode(txt);
+    messageElement.classList.add(className);
+    messageElement.append(txtNode);
+    chatBody.append(messageElement);
+
+};
+
+const getChatbotResponse = (userInput) => {
+    return responseObj[userInput] == undefined
+    ? "Nudaw"
+    : responseObj[userInput];
+};
+
+const setScrollPos = () =>{
+    if(chatBody.scrollHeight>0){
+        chatBody.scrollTop = chatBody.scrollHeight;
+    }
+}
+
+const toggleLoading = (show) => loadingElement.classList.toggle("hide", show)
